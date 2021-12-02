7cbdc7cff24e4bdf0df8727b8990de170ef495d0
maoserr <maoserr@gmail.com>
2021-12-02 12:19:24 -0600

    initial

--- src/scenes/main-scene.ts
+++ src/scenes/main-scene.ts
@@ -1,3 +1,5 @@
+import Particle = Phaser.GameObjects.Particles.Particle;
+
 interface IImageConstructor {
     scene: Phaser.Scene;
     x: number;
@@ -6,15 +8,36 @@ interface IImageConstructor {
     frame?: string | number;
 }
 
+interface ShipSpecs {
+    max_acceleration: number,
+    max_velocity: number,
+    max_angular_acceleration: number,
+    max_angular_velocity: number,
+    mass: number
+}
+
+interface ShipState {
+
+}
+
+interface ShipInput {
+    accelerate: boolean,
+    brake: boolean,
+    turn_left: boolean,
+    turn_right: boolean
+}
+
+let debug_txt: Phaser.GameObjects.Text;
 
 class Ship extends Phaser.GameObjects.Image {
     body: Phaser.Physics.Arcade.Body;
+    spec: ShipSpecs;
 
-    constructor(aParams: IImageConstructor) {
+    constructor(aParams: IImageConstructor, ship_specs: ShipSpecs) {
         super(aParams.scene, aParams.x, aParams.y, aParams.texture, aParams.frame);
 
         this.initSprite();
-        this.initPhysics();
+        this.initPhysics(ship_specs);
         this.scene.add.existing(this);
     }
 
@@ -22,11 +45,36 @@ class Ship extends Phaser.GameObjects.Image {
         this.setScale(0.8);
     }
 
-    private initPhysics() {
+    private initPhysics(ship_specs: ShipSpecs) {
         this.scene.physics.world.enable(this);
         this.body.setVelocity(0, 0);
-        this.body.setBounce(1, 1);
         this.body.setCollideWorldBounds(true);
+        this.body.setMaxSpeed(ship_specs.max_velocity);
+    }
+
+    handleInputs(ship_input: ShipInput, delta: number) {
+        let angle = this.rotation;
+        let x_rat = Math.sin(angle);
+        let y_rat = -Math.cos(angle);
+        if (ship_input.turn_left && !ship_input.turn_right) {
+            this.body.setAngularAcceleration(-200)
+        } else if (ship_input.turn_right && !ship_input.turn_left) {
+            this.body.setAngularAcceleration(200);
+        } else {
+            this.body.setAngularAcceleration(0)
+            this.body.setAngularVelocity(0)
+        }
+        if (ship_input.accelerate && !ship_input.brake) {
+            this.setFrame("ship2.png")
+            this.body.setAcceleration(x_rat * 20, y_rat * 20)
+        } else if (ship_input.brake && !ship_input.accelerate) {
+            this.setFrame("ship2.png")
+            this.body.setAcceleration(-x_rat * 20, -y_rat * 20)
+        } else {
+            this.setFrame("ship.png")
+            this.body.setAcceleration(0, 0)
+        }
+        debug_txt.setText(`Angle: ${angle}\nX: ${x_rat}\nY: ${y_rat}\nVelocity: ${this.body.speed}`)
     }
 }
 
@@ -42,31 +90,29 @@ export class MainScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.image('ship', 'images/ship.png');
+        this.load.atlas('ship', 'images/texture.png', 'images/texture.json');
         this.load.image('redParticle', 'images/red.png');
         this.load.image('bg', 'images/stars_bg.png')
     }
 
     create(): void {
-        const particles = this.add.particles('redParticle');
-
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: {start: 0.5, end: 0},
-            blendMode: 'ADD'
-        });
 
         this.ship = new Ship({
             scene: this,
             x: 400,
             y: 300,
             texture: 'ship'
+        }, {
+            max_velocity: 200,
+            max_acceleration: 10,
+            max_angular_velocity: 10,
+            max_angular_acceleration: 1,
+            mass: 500
         });
 
-        emitter.startFollow(this.ship);
 
         this.ts = this.add.tileSprite(600, 400, 1200, 800, "bg").setScrollFactor(0)
-
+        debug_txt = this.add.text(0, 0, "test\ntest").setScrollFactor(0)
         this.camera = this.cameras.main;
         this.camera.startFollow(this.ship, false, 0.2, 0.2)
         // Set up the arrows to control the camera
@@ -76,15 +122,12 @@ export class MainScene extends Phaser.Scene {
     update(time: number, delta: number) {
         this.ts.tilePositionX += 0.01 * this.ship.body.velocity.x * delta;
         this.ts.tilePositionY += 0.01 * this.ship.body.velocity.y * delta;
-        if (this.cursors.left.isDown) {
-            this.ship.body.setVelocityX(-10);
-        } else if (this.cursors.right.isDown) {
-            this.ship.body.setVelocityX(10);
-        }
-        if (this.cursors.up.isDown) {
-            this.ship.body.setVelocityY(-10);
-        } else if (this.cursors.down.isDown) {
-            this.ship.body.setVelocityY(10);
-        }
+
+        this.ship.handleInputs({
+            accelerate: this.cursors.up.isDown,
+            brake: this.cursors.down.isDown,
+            turn_left: this.cursors.left.isDown,
+            turn_right: this.cursors.right.isDown
+        }, delta)
     }
 }
